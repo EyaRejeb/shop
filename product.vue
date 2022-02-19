6e7ac6803fff173fb4c4724c2841eaed763c7612
EyaRejeb <93796426+EyaRejeb@users.noreply.github.com>
2022-02-19 02:59:36 +0100

    Component of a product

---
+++ product.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="flex">
+            <div>
+                <img src = "{{productImage}}" />
+            </div>
+            <div>
+                <h1>{{productName}}</h1>
+                <h1>product Size: {{productSize}}</h1>
+                <h1>Color: {{productColor}}</h1>
+                <h1>{{productPrice}}</h1>
+                <h1>{{productDescription}}</h1>
+    
+                
+            </div>
+     </div>>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    productName: String,
+    productPrice: String,
+    productImage: String,
+    productColor: String,
+    productSize: String,
+    productDescription: String
+  }
+};
+</script>
